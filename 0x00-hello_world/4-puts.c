d495c913295229c44ee917a9382b6b11f2bdb343
Lunga-28 <nokulungamkize28@gmail.com>
2023-02-16 14:24:58 -0800

    Low level programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - Entry point of the program
  *
@@ -9,6 +10,6 @@
  */
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
-        return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
