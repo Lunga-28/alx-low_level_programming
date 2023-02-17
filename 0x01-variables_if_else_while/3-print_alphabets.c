1968b757ddef975aeb54317240eabb6f4cef56ca
Lunga-28 <nokulungamkize28@gmail.com>
2023-02-17 13:44:03 -0800

    Variables

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+
+int main(void)
+
+{
+
+	char tc;
+
+	for (tc = 'a'; tc <= 'z'; tc++)
+	putchar(tc);
+
+	for (tc = 'A'; tc <= 'Z'; tc++)
+	putchar(tc);
+	putchar('\n');
+
+	return (0);
+
+}
