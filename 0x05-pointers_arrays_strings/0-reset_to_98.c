563b846d55c6017b5401fef8b89a5a8e5c12c9d5
Lunga-28 <nokulungamkize28@gmail.com>
2023-02-28 03:54:29 +0200

    Pointers

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,3 +1,4 @@
+#include "main.h"
 /**
  * reset_to_98 - Updates the value pointed to by n to 98
  * @n: Pointer to an int to be updated
@@ -8,5 +9,5 @@ void reset_to_98(int *n)
 {
 
 *n = 98;
-
+_putchar('\n');
 }
