b57331db45a05733457698a669dbe066af3af8b9
Lunga-28 <nokulungamkize28@gmail.com>
2023-02-28 04:00:53 +0200

    String length

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <stdio.h>
 /**
  * reset_to_98 - Updates the value pointed to by n to 98
  * @n: Pointer to an int to be updated
@@ -9,5 +9,5 @@ void reset_to_98(int *n)
 {
 
 *n = 98;
-_putchar('\n');
+putchar('\n');
 }
