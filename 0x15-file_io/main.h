1aa8b068ab3b91a7be99f8f5c823094a7d418bdd
Lunga-28 <124337357+Lunga-28@users.noreply.github.com>
2023-05-09 03:18:52 +0200

    Update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -12,10 +12,13 @@
 #define BUFFERSIZE 1204
 
 int _putchar(char c);
-/*0-read_textfile.c*/
-ssize_t read_textfile(const char *filename, size_t letters);
-/*1-create_file.c*/
+
+ssize_t read_textfile(const char *filename, size_t letters); 
+
+
 int create_file(const char *filename, char *text_content);
+
+
 int append_text_to_file(const char *filename, char *text_content);
 
 #endif
