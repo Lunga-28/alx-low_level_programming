fa498b42fd7493cbb2d4211209e6a574e11e4c48
Lunga-28 <124337357+Lunga-28@users.noreply.github.com>
2023-10-11 18:53:24 +0200

    Create 0-linear.c

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,24 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - ...........
+ * @array: ............
+ * @size: .............
+ * @value: ............
+ * Return: ............
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t x;
+
+	if (array == NULL)
+		return (-1);
+
+	for (x = 0; x < size; x++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", x, array[x]);
+		if (array[x] == value)
+			return (x);
+	}
+	return (-1);
+}
